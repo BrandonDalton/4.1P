4f0abcc8f9033752d05308606bf7def88c1fd0da
Brandon <brandondalton1996@gmail.com>
2020-09-07 22:36:12 +1000

    4.1 -  4.2

--- server.js
+++ server.js
@@ -15,11 +15,13 @@ const base = `${__dirname}/public`
 
 const saltRounds = 10;
 
+// Mailchimp Config
 mailchimp.setConfig({
   apiKey: "dd440949bd38832bd72b4281546b7669-us17",
   server: "us17",
 });
 
+// Checks Response Is Successful
 async function callPing() {
   const response = await mailchimp.ping.get();
   console.log(response);
@@ -27,10 +29,12 @@ async function callPing() {
 
 callPing();
 
+// Mailing List ID
 const listId = "ca5e1222ed";
 
+//Add User Function To Mail
 async function addUserToMail(email, fname, lname) {
-  console.log('Running Start', email, fname, lname );
+  console.log('Running Start', email, fname, lname);
   const response = await mailchimp.lists.addListMember(listId, {
     email_address: email,
     status: "subscribed",
@@ -39,11 +43,8 @@ async function addUserToMail(email, fname, lname) {
       LNAME: lname
     }
   })
-  console.log('Running' , response);
+  console.log('Running', response);
 }
-
-
-
 
 //Database
 
@@ -105,6 +106,7 @@ const userSchema = new mongoose.Schema(
   }
 )
 
+
 const User = mongoose.model("users", userSchema);
 
 //Application
@@ -158,19 +160,133 @@ body('password').custom((value, { req }) => {
 
         user.save(err => {
           if (err) { console.log(err) }
-          else { console.log("Successfull!")
-          // addUserToMail(user.email, user.fname, user.lname);
-          addUserToMail(user.email, user.fname, user.lname);
-          return res.redirect('login.html')
-        }
+          else {
+            console.log("Successfull!")
+            // addUserToMail(user.email, user.fname, user.lname);
+            addUserToMail(user.email, user.fname, user.lname);
+            return res.redirect('login.html')
+          }
         })
       })
     })
   }
 });
 
+app.route('/workers')
+  //Retrieves All Workers
+  .get((req, res) => {
+    User.find((err, workerList) => {
+      if (!err) {
+        console.log('Workers:', workerList);
+        res.send('Good Job')
+      } else {
+        console.log('Errors')
+      }
+    });
+  })
+  //Add Worker
+  .post((req, res) => {
+
+    const password = req.body.password;
+    //Password Hasing Storing
+
+    //Hashes The Password
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      bcrypt.hash(password, salt, function (err, hash) {
+        //If No Errors  Create User
+        const user = new User(
+          {
+            _id: userID,
+            fname: req.body.fname,
+            lname: req.body.lname,
+            email: req.body.email,
+            password: hash,
+            address: req.body.address,
+            city: req.body.city,
+            state: req.body.state,
+            postcode: req.body.postcode,
+            mobile: req.body.mobile
+          }
+        )
+
+        user.save(err => {
+          if (err) { console.log(err) }
+          else {
+            return res.send("User Added")
+          }
+        })
+      })
+    })
+  })
+  //Delete All Users
+  .delete((req, res) => {
+    User.deleteMany((err) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send('Tasks All Deleted')
+      }
+    })
+  })
+//Route With ID
+app.route('/workers/:id')
+  .get((req, res) => {
+    //Find User Based On ID
+    User.findOne({ _id: req.params._id }, (err, foundWorker) => {
+      if (foundWorker) {
+        res.send(foundWorker)
+      } else {
+        res.send("No Worker Found");
+      }
+    })
+  })
+  //Delete Specific User Users
+  .delete((req, res) => {
+    User.deleteOne({ _id: req.params._id }, (err, workerDeleted) => {
+      if (workerDeleted) {
+        res.send(workerDeleted)
+      } else {
+        res.send("Worker Not Found")
+      }
+    })
+  })
+  //Updates Users Address and Mobile Based on ID
+  .put((req, res) => {
+    User.update(
+      { _id: req.params._id },
+      { address: req.body.address },
+      { mobile: req.body.mobile },
+      { overwrite: true }, (err) => {
+        if (err) {
+          res.send(err)
+        } else {
+          res.send("Worker Updated")
+        }
+      }
+    )
+  })
+  //Updates Users Password
+  .patch((req, res) => {
+    const password = req.body.password;
+    //Password Hasing Storing
+
+    //Hashes The Password
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      bcrypt.hash(password, salt, function (err, hash) {
+        User.update(
+          { _id: req.params._id },
+          { password: password },
+          (err) => {
+            if (!err) { res.send('Successfully updated! ') }
+            else res.send(err)
+          }
+        )
+      })
+    })
+  })
+
 app.post('/loginForm', function (req, res) {
-  
+
   //User Login Email Input
   const userEmailLogin = req.body.email;
 
@@ -178,32 +294,33 @@ app.post('/loginForm', function (req, res) {
   User.findOne({ email: userEmailLogin }, function (err, acc) {
     //If Errors Log
     if (err) {
-      console.log("Errors:" ,err)
+      console.log("Errors:", err)
     }
     else {
       //If Document Is Null
-      if(acc == null) {
+      if (acc == null) {
         //Cant Find Account
-        console.log("Errors:" , 'Cant Find Login Details')
+        console.log("Errors:", 'Cant Find Login Details')
       } else {
         //Input Password
         const passwordInput = req.body.password;
         //Compare Database Has vs Password
-        bcrypt.compare(passwordInput, acc.password, function(err, result) {
+        bcrypt.compare(passwordInput, acc.password, function (err, result) {
           //If Correct Redirect
-          if(result == true){
+          if (result == true) {
             res.redirect('reqlogin.html')
           } else {
             //Password is incorrect
             console.log('Password Is Incorrect.')
           }
-      });
+        });
       }
-      
+
     }
   });
 })
 
+//App Listen
 app.listen(port, function () {
   console.log('Server Is Running On Port 3000');
   console.log(base);
